08f97f169e831ad865ac7990e3229c56926b15e2
Peter Sebestyen <petersebestyen@MacBook-Air-von-Peter.local>
2024-01-16 18:38:40 +0100

    Started making the dropdown menu - Problem with JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-/*Projects*/
+/*Navigation Dropdownmenu*/
+
+let icon = document.getElementById('dropdownicon');
+let menu = document.getElementById('dropdownMenu');
+
+icon.addEventListener('click', function(){
+    return menu.style.display = 'flex';
+});
+
+function dropdown(){
+    return menu.style.display = 'flex';
+}
