ef04afa0c85f1e557ebe0f9f3c1f3b9e8e2ee009
Peter Sebestyen <petersebestyen@MacBook-Air-von-Peter.local>
2024-01-18 22:39:01 +0100

    Added working dropdown menu

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,16 @@
 
 let icon = document.getElementById('dropdownicon');
 let menu = document.getElementById('dropdownMenu');
-
-icon.addEventListener('click', function(){
-    return menu.style.display = 'flex';
-});
+let closeIcon = document.getElementById('dropdown_close');
 
 function dropdown(){
+    icon.style.display = 'none';
+    closeIcon.style.display = 'flex';
     return menu.style.display = 'flex';
+};
+
+function closeDropdown(){
+    closeIcon.style.display = 'none';
+    icon.style.display = 'flex';
+    return menu.style.display = 'none';
 }
